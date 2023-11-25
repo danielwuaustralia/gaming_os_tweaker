f7d421383c7e7cb3a2f28d67d97a1e15df54bc2c
Douglas Galdino <dougg0k@gmail.com>
2023-11-25 17:38:21 -0300

    Update replace_startmenu.cmd

--- scripts/debloat/replace_startmenu.cmd
+++ scripts/debloat/replace_startmenu.cmd
@@ -20,15 +20,15 @@ taskkill /f /im SearchHost.exe
 move MicrosoftWindows.Client.CBS_cw5n1h2txyewy MicrosoftWindows.Client.CBS_cw5n1h2txyewy.old
 
 taskkill /f /im backgroundTaskHost.exe
-taskkill /f /im ctfmon.exe
-taskkill /f /im SearchUI.exe
 call %~dp0\..\optional_helpers\run_minsudo "move %windir%\System32\backgroundTaskHost.exe %windir%\System32\backgroundTaskHost.exe.old"
+taskkill /f /im ctfmon.exe
 call %~dp0\..\optional_helpers\run_minsudo "move %windir%\System32\ctfmon.exe %windir%\System32\ctfmon.exe.old"
+taskkill /f /im SearchUI.exe
 move Microsoft.Windows.Cortana_cw5n1h2txyewy\SearchUI.exe Microsoft.Windows.Cortana_cw5n1h2txyewy\SearchUI.exe.old
 
 taskkill /f /im RuntimeBroker.exe
-taskkill /f /im ShellExperienceHost.exe
 call %~dp0\..\optional_helpers\run_minsudo "move %windir%\System32\RuntimeBroker.exe %windir%\System32\RuntimeBroker.exe.old"
+taskkill /f /im ShellExperienceHost.exe
 call %~dp0\..\optional_helpers\run_minsudo "move %windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe %windir%\SystemApps\ShellExperienceHost_cw5n1h2txyewy\ShellExperienceHost.exe.old"
 
 taskkill /f /im DataExchangeHost.exe
@@ -36,6 +36,9 @@ call %~dp0\..\optional_helpers\run_minsudo "move %windir%\System32\DataExchangeH
 
 taskkill /f /im mobsync.exe
 call %~dp0\..\optional_helpers\run_minsudo "move %windir%\System32\mobsync.exe %windir%\System32\mobsync.exe.old"
+
+taskkill /f /im GameBarPresenceWriter.exe
+call %~dp0\..\optional_helpers\run_minsudo "move %windir%\System32\GameBarPresenceWriter.exe %windir%\System32\GameBarPresenceWriter.exe.old"
 
 :: Additionals
 move Microsoft.XboxGameCallableUI_cw5n1h2txyewy Microsoft.XboxGameCallableUI_cw5n1h2txyewy.old
